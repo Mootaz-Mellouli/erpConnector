17edf3750fe03f39f6e19770fd6da1b0f20e3157
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2022-04-11 15:30:48 +0100

    Initial commit

---
+++ src/main/java/com/example/erpConnector/ErpConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.erpConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpConnectorApplication.class, args);
+	}
+
+}
